7087b8ea087f2e3090bb051359ee420a4ac695af
hertz <zhanghelook@outlook.com>
2019-06-17 21:59:58 +0800

    feat(main.rs): hello, i'm zhanghe!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-
 fn main() {
-    let stdout = stdout();
-    let out = b"Hello fellow Rustaceans!";
-    let width = 24;
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    println!("Hello, I'm zhanghe!");
 }
