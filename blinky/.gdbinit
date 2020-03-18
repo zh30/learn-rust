bdffbff3efae8e76f13469c2537cccce68c855e4
zhanghe <zhanghecool@outlook.com>
2020-03-18 18:11:24 +0800

    feat(config): add .cargo .gdbinit .vscode openocd.cfg

---
+++ blinky/.gdbinit
@@ -0,0 +1,4 @@
+file ./target/thumbv7m-none-eabi/debug/blinky
+target remote :3333
+monitor reset halt
+load
--- blinky/src/main.rs
+++ blinky/src/main.rs
@@ -3,43 +3,23 @@
 
 extern crate panic_halt;
 extern crate stm32f103xx_hal as hal;
-#[macro_use]
-extern crate nb;
 
 use cortex_m_rt::entry;
 
-use hal::prelude::*;
-use hal::stm32f103xx;
-use hal::timer::Timer;
-
 #[entry]
 fn main() -> ! {
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32f103xx::Peripherals::take().unwrap();
+    let mut n = 0;
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-
-    // 设置时钟总线
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    // 设置通用引脚 (GPIO)
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-
-    // LED 对应的 PC13 引脚
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-    // 淘宝上有些版本的核心板的 LED 会接在 PB12 引脚上，这样的话用下面两行替换
-    // let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
-    // let mut led = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
-
-    let mut timer = Timer::syst(cp.SYST, 1000.hz(), clocks);
     loop {
-        block!(timer.wait()).unwrap();
-        // 点亮 LED
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        // 关闭 LED
-        led.set_low();
+        let _fib = fib(n);
+        n += 1;
+    }
+}
+
+fn fib(n: usize) -> usize {
+    if n < 2 {
+        1
+    } else {
+        fib(n - 1) + fib(n - 2)
     }
 }
