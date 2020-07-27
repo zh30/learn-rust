4fc0293b4243d1072aea165751ea5dbdfc37c950
He Zhang <zhanghecool@outlook.com>
2020-07-27 22:55:36 +0800

    feat(ownership): 增加部分借用代码

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,7 +8,13 @@ fn main() {
     // println!("{:?}", place2);
 
     // Borrow
-    let a = [1,2,3];
+    let a = [1, 2, 3];
     let b = &a;
     println!("{:p}", b);
+    let mut c = vec![1, 2, 3];
+    let d = &mut c;
+    d.push(4);
+    println!("{:?}", d);
+    let e = &42;
+    assert_eq!(42, *e);
 }
