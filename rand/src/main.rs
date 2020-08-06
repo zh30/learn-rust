c7f2b08778077c551d6ba2b06e2249255bd8ce57
zhanghe <zhanghecool@outlook.com>
2020-08-06 19:48:54 +0800

    feat(rand): rand guess

--- rand/src/main.rs
+++ rand/src/main.rs
@@ -1,8 +1,14 @@
-// use std::io;
 use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Falled to read line");
+    println!("You guessed: {}", guess);
 }
