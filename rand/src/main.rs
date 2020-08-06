9d0807df0c30f5a2fb736a9feb428c18a8971bfa
zhanghe <zhanghecool@outlook.com>
2020-08-06 18:36:49 +0800

    feat(rand): rand number

---
+++ rand/src/main.rs
@@ -0,0 +1,8 @@
+// use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+}
