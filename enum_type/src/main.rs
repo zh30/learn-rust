fad339e94de325bd435b97d296b104fd3177fe80
He Zhang <zhanghecool@outlook.com>
2020-09-11 17:19:37 +0800

    feat(enum_type): 完善 enum

--- enum_type/src/main.rs
+++ enum_type/src/main.rs
@@ -1,30 +1,54 @@
 fn main() {
-    // let home = IpAddr {
-    //     kind: IpAddrKind::V4,
-    //     address: String::from("192.168.0.1"),
-    // };
-    // let loopback = IpAddr {
-    //     kind: IpAddrKind::V6,
-    //     address: String::from("::1"),
-    // };
+	// let home = IpAddr {
+	//     kind: IpAddrKind::V4,
+	//     address: String::from("192.168.0.1"),
+	// };
+	// let loopback = IpAddr {
+	//     kind: IpAddrKind::V6,
+	//     address: String::from("::1"),
+	// };
 
-    let home = IpAddrKind::V4(String::from("127.0.0.1"));
-    let loopback = IpAddrKind::V6(String::from("::1"));
+	let _home = IpAddrKind::V4(127, 0, 0, 1);
+	let _loopback = IpAddrKind::V6(String::from("::1"));
 
-    println!("The home ipaddr is {}", home);
-    println!("The loopback ipaddr is {}", loopback);
-}
+	// println!("The home ipaddr is {}", home);
+	// println!("The loopback ipaddr is {}", loopback);
 
-// #[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+	let m = Message::Write(String::from("hello"));
+	m.call();
 }
 
 #[derive(Debug)]
+struct IpAddr {
+	kind: IpAddrKind,
+	address: String,
+}
+
+struct QuitMessage;
+struct MoveMessage {
+	x: i32,
+	y: i32,
+}
+struct WriteMessage(String);
+struct ChangeColorMessage(i32, i32, i32);
+
+#[derive(Debug)]
 enum IpAddrKind {
-    V4(String),
-    V6(String),
+	V4(u8, u8, u8, u8),
+	V6(String),
+}
+
+enum Message {
+	Quit,
+	Move { x: i32, y: i32 },
+	Write(String),
+	ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+	fn call(&self) {
+		println!("The message");
+	}
 }
 
 fn route(ip_type: IpAddrKind) {}
