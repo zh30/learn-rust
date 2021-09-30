9cbb31631c5a880033d26a23bbdb790314d25ff8
张赫 <he.zhang@vcg.com>
2021-09-30 11:39:59 +0800

    feat(fn): add fn

--- fn/src/main.rs
+++ fn/src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)]
+pub struct Student {
+    name: &'static str,
+    score: i32,
+}
+
+impl Student {
+    pub fn new(name: &'static str, score: i32) -> Self {
+        Student {name, score}
+    }
+
+    pub fn get_name(&self) -> &str {
+        self.name
+    }
+
+    pub fn get_score(&self) -> i32 {
+        self.score
+    }
+
+    pub fn set_name(&mut self, name: &'static str) {
+        self.name = name;
+    }
+
+    pub fn set_score(&mut self, score: i32) {
+        self.score = score;
+    }
+}
+
 fn main() {
     hi();
     let a: i32 = -1;
@@ -8,6 +36,10 @@ fn main() {
     let r = hi3(a, b);
     println!{"R = {}", r};
     println!("Hello, world!");
+    let mut student: Student = Student::new("zhanghe", 29);
+    println!("name: {}, score: {}", student.get_name(), student.get_score());
+    student.set_score(30);
+    println!("student is {:?}", student);
 }
 
 fn hi() {
