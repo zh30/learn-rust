f11d298aa9be758ae4e4e33a4c27cc8a17982109
He Zhang <zhanghecool@outlook.com>
2023-03-29 17:24:21 +0800

    docs(hello): 24 to 20

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -4,7 +4,7 @@ use std::io::{stdout, BufWriter};
 fn main() {
     let stdout = stdout();
     let out = b"Hello, zhanghe.cool!";
-    let width = 24;
+    let width = 20;
 
     let mut writer = BufWriter::new(stdout.lock());
     say(out, width, &mut writer).unwrap();
