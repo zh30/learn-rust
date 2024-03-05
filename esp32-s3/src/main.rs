d40354347885b0c7677a050f2eb552945a1141f8
Henry Zhang <hello@zhanghe.dev>
2024-03-06 00:20:40 +0800

    Add new files and update dependencies

---
+++ esp32-s3/src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use esp32s3_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, IO};
+use esp_backtrace as _;
+use esp_println::println;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
+
+    println!("Hello world!");
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio46.into_push_pull_output();
+    led.set_high().unwrap();
+
+    loop {
+        println!("Loop...");
+        led.toggle().unwrap();
+        delay.delay_ms(500u32);
+    }
+}
--- nrf52840/src/main.rs
+++ nrf52840/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[entry]
+fn main() -> ! {
+    loop {
+        // Your code goes here
+    }
 }
